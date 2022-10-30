e7c38938969d0e338971c2dbb78087c0c18f78d6
gina622 <gzheng1@babson.edu>
2022-10-30 19:17:48 -0400

    Project First Version

---
+++ js/navigation.js
@@ -0,0 +1,10 @@
+document.write(` 
+<a href="index.html"><img class=" favicon" src="images/android-chrome-192x192.png" alt="favicon" title="favicon"></a>
+<div id="nav-container">
+    <a class="nav-link" href="index.html">Home</a>
+    <a class="nav-link" href="entertainment.html">Entertainment</a>
+    <a class="nav-link" href="cooking.html">Cooking</a>
+    <a class="nav-link" href="aesthetics.html">Aesthetics</a>
+    <a class="nav-link" href="contact.html">Contact Me</a>
+    <a class="nav-link" href="sitemap.html">Sitemap</a>
+</div>`);
